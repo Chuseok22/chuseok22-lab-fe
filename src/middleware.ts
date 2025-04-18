0f9d99983e1249c7b26b8d8884592aa58a0499fb
Chuseok22 <bjh59629@naver.com>
2025-04-18 15:44:00 +0900

    Github Issue Helper 기능 구현 : feat : URL 이동시 인증 상태에 따라 접근 권한 설정 middleware.ts 파일 추가 https://github.com/Chuseok22/chuseok22-lab-fe/issues/11

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { AUTH_PATHS, PUBLIC_PATHS } from "@/lib/api/auth/auth.type";
+
+export function middleware(request: NextRequest) {
+  const pathname: string = request.nextUrl.pathname;
+
+  // 인증 상태 확인
+  const accessToken: string | undefined = request.cookies.get('accessToken')?.value;
+
+  // 이미 인증된 사용자는 로그인 & 회원가입 페이지 접근 불가
+  if (accessToken && AUTH_PATHS.includes(pathname)) {
+    console.log("이미 로그인 된 사용자입니다.");
+    const url = new URL('/', request.url);
+    return NextResponse.redirect(url);
+  }
+
+  // 공개 경로는 인증 생략
+  if (PUBLIC_PATHS.includes(pathname)) {
+    console.log("공개 경로 접근");
+    return NextResponse.next();
+  } else { // 그 외 경로는 접근 시 인증 확인
+    if (!accessToken) {
+      console.log("로그인이 필요합니다.");
+      const url = new URL('/auth/login', request.url);
+      return NextResponse.redirect(url);
+    }
+  }
+  return NextResponse.next();
+}
+
+// 미들웨어 적용 경로
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|fonts|images|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+}
