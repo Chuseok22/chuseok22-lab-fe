92c4cfd7486ac9d79c022e841927e0455a0f8a4c
Chuseok22 <bjh59629@naver.com>
2025-05-01 16:56:38 +0900

    홈 화면 사용자 정보 출력 : feat : SSR, CSR 분리에 따른 SSR fetch 기본 코드 작성 https://github.com/Chuseok22/chuseok22-lab-fe/issues/34

---
+++ src/lib/api/common/http/serverFetch.ts
@@ -0,0 +1,29 @@
+import { cookies } from "next/headers";
+import { API_BASE_URL } from "@/lib/api/common/http/axios.type";
+
+export async function serverFetch<T>(
+    path: string,
+    options: RequestInit = {}
+): Promise<T> {
+  // 서버 컨텍스트에서만 동기적으로 작동
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore
+  .getAll()
+  .map((cookie) => `${cookie.name}=${cookie.value}`)
+  .join('; ');
+
+  const response = await fetch(`${API_BASE_URL}${path}`, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...(cookieHeader ? {Cookie: cookieHeader} : {}),
+      ...options.headers,
+    },
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error("SSR 요청 중 오류 발생");
+  }
+  return response.json();
+}
