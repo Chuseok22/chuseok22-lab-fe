d19ac1322e137949405ae0a405bb7a011c3a060c
Chuseok22 <bjh59629@naver.com>
2025-05-01 16:55:05 +0900

    홈 화면 사용자 정보 출력 : feat : layout.tsx 적용에 따른 페이지 분리 https://github.com/Chuseok22/chuseok22-lab-fe/issues/34

--- src/app/(no-header)/auth/login/page.tsx
+++ src/app/(no-header)/auth/login/page.tsx
@@ -1,4 +1,4 @@
-import '../../globals.css'
+import '../../../globals.css'
 import Link from "next/link";
 import { FaFacebook, FaGoogle, FaInstagram } from 'react-icons/fa'
 import LoginForm from "@/components/auth/login/LoginForm";
