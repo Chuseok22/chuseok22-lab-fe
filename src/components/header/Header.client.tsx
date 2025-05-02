4e7680b1d0fe1a1c7073aace191316805a67c13d
Chuseok22 <bjh59629@naver.com>
2025-05-02 14:02:00 +0900

    홈 화면 사용자 정보 출력 : feat : 헤더 토글 함수 리팩토링 https://github.com/Chuseok22/chuseok22-lab-fe/issues/34

--- src/components/header/Header.client.tsx
+++ src/components/header/Header.client.tsx
@@ -1,19 +1,21 @@
 'use client'
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Image from "next/image";
 import SideMenuBar from "@/components/header/SideMenuBar";
 
 const HeaderClient = () => {
   const [open, setOpen] = useState(false);
+  const toggleSidebar = useCallback(() => setOpen(prev => !prev), []);
+
   return (
       <>
         <div className='lg:hidden flex justify-end px-4'>
-          <button onClick={() => setOpen(prev => !prev)}>
+          <button onClick={toggleSidebar}>
             <Image src='/menu-rounded.svg' alt='menu' width='20' height='20'/>
           </button>
         </div>
-        <SideMenuBar isSidebarOpen={open} toggleSidebar={() => setOpen(prev => !prev)}/>
+        <SideMenuBar isSidebarOpen={open} toggleSidebar={toggleSidebar}/>
       </>
   )
 }
